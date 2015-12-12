644418164eb6c26c8097256dcb00e2bb039cc69d
Shelley Hicklin <shelleyhicklin@hotmail.com>
2015-12-12 21:19:27 +0000

    add todo item model and specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -67,3 +67,17 @@ end
   # [...] snippet to continue making this method available in RSpec 2.99 and RSpec 3:
   c.expose_current_running_example_as :example
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    # Choose a test framework:
+    with.test_framework :rspec
+
+    # Choose one or more libraries:
+    #with.library :active_record
+    #with.library :active_model
+    #with.library :action_controller
+    # Or, choose the following (which implies all of the above):
+    with.library :rails
+  end
+end
